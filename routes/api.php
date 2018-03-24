6c1d06d965bb2758e282ecaf3b7513099fd7b044
CHIGY <gdavid.ptorrez@gmail.com>
2018-03-24 00:39:42 -0400

    Instalando Dingo/api

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,17 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', function ($api) {
+
+    $api->get('docs', function () {
+        return 'Documentacion de la API Rest';
+    });
+
 });
